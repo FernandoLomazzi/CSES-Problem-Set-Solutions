743a4a52e8842241c0a3b576344a7f35973aaf5b
Lascot29 <78239004+Lascot29@users.noreply.github.com>
2021-02-22 15:24:43 -0300

    Add files via upload

---
+++ Dice Combinations.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1000000007;
+int main() {
+	#ifdef w1b 
+	freopen("input.in", "r", stdin);
+	//freopen("output.out" , "w", stdout);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	vector<int> v(max(n+1,20));
+	for(int i=1;i<=6;i++)
+		v[i] = 1;
+	for(int i=1;i<=n;i++){
+		for(int j=i+1;j<=n and j<=i+6;j++){
+			v[j] = ((long long)v[j]+v[i])%mod;
+		}
+	}
+	cout << v[n] << '\n';
+	return 0;
+}
