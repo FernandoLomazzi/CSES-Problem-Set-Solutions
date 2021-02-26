53daf29fe73dd98fb94bbc55f9df35b14c21b12e
Lascot29 <78239004+Lascot29@users.noreply.github.com>
2021-02-26 15:17:41 -0300

    Add files via upload

---
+++ Problemas.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	#ifdef w1b 
+	freopen("input.in", "r", stdin);
+	//freopen("output.out" , "w", stdout);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n,k;
+	cin>>n>>k;
+	vector<int> v(n);
+	for(int i=0;i<n;i++)
+		cin>>v[i];
+	sort(v.begin(),v.end());
+	int a=0,b=n-1,ans=0;
+	while(b-a>=0){
+		if((long long)v[a]+v[b]>k)
+			b--;
+		else{
+			a++;
+			b--;
+		}
+		ans++;
+	}
+	cout << ans << '\n';
+	return 0;
+}
+
