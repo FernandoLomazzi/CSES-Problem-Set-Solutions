8d0763a709bbe30d7fb80e98c023cd0dcf8b7fe2
Lascot29 <78239004+Lascot29@users.noreply.github.com>
2021-02-28 23:08:35 -0300

    Add files via upload

---
+++ Problemas.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct event{
+	int t,v;
+	bool operator<(event o){
+		return(this->t<o.t);
+	}
+};
+int main() {
+	#ifdef w1b 
+	freopen("input.in", "r", stdin);
+	//freopen("output.out" , "w", stdout);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	vector<event> v(2*n);
+	for(int i=0;i<n;i++){
+		int a,b;
+		cin>>a>>b;
+		v[2*i] = {a,1};
+		v[2*i+1] = {b,-1};
+	}
+	sort(v.begin(),v.end());
+	int p=0,ans=0;
+	for(auto &it: v){
+		p += it.v;
+		ans = max(ans,p);
+	}
+	cout << ans << '\n';
+	
