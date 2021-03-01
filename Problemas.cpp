546813bdb0acd013e42d33a4b183dbbf6762f6f1
Lascot29 <78239004+Lascot29@users.noreply.github.com>
2021-02-28 23:07:50 -0300

    Add files via upload

---
+++ Problemas.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	#ifdef w1b 
+	freopen("input.in", "r", stdin);
+	//freopen("output.out" , "w", stdout);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n,m;
+	cin>>n>>m;
+	multiset<int> st;
+	for(int i=0;i<n;i++){
+		int x;
+		cin>>x;
+		st.insert(x);
+	}
+	for(int i=0;i<m;i++){
+		int k;
+		cin>>k;
+		auto it = st.upper_bound(k);
+		if(it==st.begin())
+			cout << -1 << '\n';
+		else{
+			cout << *--it << '\n';
+			st.erase(it);
+		}
+	}
+	return 0;
+}
